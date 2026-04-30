56e60bfc0976214e6406f21056794bb8cf98bc89
Wiktor <wiktor.markowski81@gmail.com>
2026-04-30 16:56:34 +0200

    Fix params and rename namespace to be more idiomatic

--- backend/app/Models/HomeModel.php
+++ backend/app/Models/HomeModel.php
@@ -1,25 +1,28 @@
 <?php
 
-namespace App\Model;
+namespace App\Models;
 
 use CodeIgniter\Model;
 
 class HomeModel extends Model
 {
-	public function createUser($firstname, $lastname, $email, $password, $group = 1)
+	public function createUser($firstname, $lastname, $email, $password, $userGroup = 0)
 	{
-		$params = [];
-		$params[] = ["first_name" => $firstname];
-		$params[] = ["last_name"  => $lastname];
-		$params[] = ["email"      => $email];
-		$params[] = ["password"   => $password];
-		$params[] = ["group"      => $group];
+		$params = [
+			"first_name" => $firstname,
+			"last_name"  => $lastname,
+			"email"      => $email,
+			"password"   => $password,
+			"user_group" => $userGroup
+		];
 
 		$this->db->query("
 			INSERT INTO `users`
-				(`id`, `first_name`, `last_name`, `email`, `password`, `group`)
+				(`first_name`, `last_name`, `email`, `password`, `user_group`)
 				VALUES
-				(:first_name:, :last_name:, :email:, :password:, :group:)
+				(:first_name:, :last_name:, :email:, :password:, :user_group:)
 		", $params);
+
+		return $this->db->affectedRows();
 	}
 }
