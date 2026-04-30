cc7780be89ec097aa3263f216f23c8959561d695
Wiktor <wiktor.markowski81@gmail.com>
2026-04-30 10:05:49 +0200

    Add migration to create users table

---
+++ backend/app/Database/Migrations/2026-04-30-073824_CreateUserTable.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateUserTable extends Migration
+{
+	public function up()
+	{
+		$this->db->query("
+			CREATE TABLE `users` (
+				`id`         int          NOT NULL,
+				`first_name` varchar(255) NOT NULL,
+				`last_name`  varchar(255) NOT NULL,
+				`email`      varchar(255) NOT NULL,
+				`password`   text         NOT NULL,
+				`group`      smallint(2)  NOT NULL
+			) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+		");
+
+		$this->db->query("
+			ALTER TABLE `users`
+				ADD PRIMARY KEY (`id`);
+		");
+
+		$this->db->query("
+			ALTER TABLE `users`
+				MODIFY `id` int NOT NULL AUTO_INCREMENT;
+		");
+	}
+
+	public function down()
+	{
+	}
+}
