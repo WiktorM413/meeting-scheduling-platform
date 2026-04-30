ebba7ef1b377f8eba9366e474f772e11f73fa7a2
Wiktor <wiktor.markowski81@gmail.com>
2026-04-30 08:39:16 +0200

    Typicize setter function in FormField

--- frontend/src/Components/FormField.tsx
+++ frontend/src/Components/FormField.tsx
@@ -1,17 +1,17 @@
-type FormFieldProps =
+type FormFieldProps<T extends string|number> =
 {
 	label:      string;
-	value:      string|number;
-	setter:     any;
+	value:      T;
+	setter:     React.Dispatch<React.SetStateAction<T>>;
 	inputType?: string;
 };
 
-export default function FormField({ label, value, setter, inputType = "text" }: FormFieldProps)
+export default function FormField<T extends string|number>({ label, value, setter, inputType = "text" }: FormFieldProps<T>)
 {
 	return (
 		<div className="msp-form-field">
 			<p>{label}</p>
-			<input type={inputType} value={value} onChange={(e) => {setter(e.target.value)}}/>
+			<input type={inputType} value={value} onChange={(e) => {setter(e.target.value as T)}}/>
 		</div>
 	);
 }
