f62a2d629b389c90e0ea3c921dcc75bf42f5c033
Wiktor <wiktor.markowski81@gmail.com>
2026-05-01 15:11:51 +0200

    Set password input type to type password

--- frontend/src/Home/Login.tsx
+++ frontend/src/Home/Login.tsx
@@ -41,8 +41,8 @@ export default function Login()
 				Log in
 			</h1>
 			<div className="msp-login-form">
-				<FormField label="Email"    value={email}    setter={setEmail} inputType="email"/>
-				<FormField label="Password" value={password} setter={setPassword}/>
+				<FormField label="Email"    value={email}    setter={setEmail}    inputType="email"/>
+				<FormField label="Password" value={password} setter={setPassword} inputType="password"/>
 				<button className="msp-button msp-login-submit" onClick={SubmitData}>Log in</button>
 
 				<div className="msp-small-text">
