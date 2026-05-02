49d9568cdfdb4478039a341a47a2ac6d192f4590
Wiktor <wiktor.markowski81@gmail.com>
2026-05-02 16:40:09 +0200

    Add className to form field props

--- frontend/src/Components/MspFormField.tsx
+++ frontend/src/Components/MspFormField.tsx
@@ -6,12 +6,13 @@ type FormFieldProps<T extends string|number> =
 	value:      T;
 	setter:     React.Dispatch<React.SetStateAction<T>>;
 	inputType?: string;
+	className?: string;
 };
 
-export default function MspFormField<T extends string|number>({ label, value, setter, inputType = "text" }: FormFieldProps<T>)
+export default function MspFormField<T extends string|number>({ label, value, setter, inputType = "text", className }: FormFieldProps<T>)
 {
 	return (
-		<div className="msp-form-field">
+		<div className={"msp-form-field " + className}>
 			<p>{label}</p>
 			<input type={inputType} value={value} onChange={(e) => {setter(e.target.value as T)}}/>
 		</div>
