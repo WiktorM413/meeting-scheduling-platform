f8d44a2a954a68c810adba186928ea03bc599ca7
Wiktor <wiktor.markowski81@gmail.com>
2026-05-02 15:04:11 +0200

    Add account buttons

--- frontend/src/Templates/Header/Header.tsx
+++ frontend/src/Templates/Header/Header.tsx
@@ -1,12 +1,16 @@
+import "./style.scss";
+
 import { useAuth } from "../../context/AuthContext";
+import AccountButtons from "./AccountButtons";
 
 export default function Header()
 {
 	const { isAuthenticated } = useAuth();
 
 	return (
-		<>
+		<div className="msp-header">
 			{isAuthenticated == true ? "yes" : "no"}
-		</>
+			<AccountButtons isAuthenticated={isAuthenticated}/>
+		</div>
 	)
 }
