19c4019b9d658d0b5d7fa2c753218bef28da068b
Wiktor <wiktor.markowski81@gmail.com>
2026-05-05 10:16:31 +0200

    Add base template for index

--- frontend/src/Home/Index.tsx
+++ frontend/src/Home/Index.tsx
@@ -1,11 +1,33 @@
-import { useAuth } from "../context/AuthContext";
-
+import MspButton from "../Components/MspButton";
 
 export default function Index()
 {
-	const { userData } = useAuth();
-
 	return (
-		<><p>Hello {userData?.firstname} {userData?.lastname}. You're a {userData?.userGroup?.type}</p></>
+		<div className="msp-home">
+			<section className="msp-home-hero">
+				<div className="msp-home-headers">
+					<h1>Scheduling made simple</h1>
+					<h2>Set your availability, let people book time, and keep everything organized across time zones.</h2>
+				</div>
+				<div className="msp-home-button-group">
+					<MspButton label="Get started"/>
+					<MspButton label="See how it works"/>
+				</div>
+			</section>
+			<section className="msp-home-preview">
+				<div className="msp-home-calendar">
+					{/* TODO: Create a calendar element for display */}
+					Calender
+				</div>
+			</section>
+			<section className="msp-home-how-it-works">
+				<h2>How it works</h2>
+				<ol>
+					<li>Set availability</li>
+					<li>Share booking link</li>
+					<li>Get appointments</li>
+				</ol>
+			</section>
+		</div>
 	);
 }
