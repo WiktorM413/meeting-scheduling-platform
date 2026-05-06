812d074364c09cd795f7f3365986f6772da0045d
Wiktor <wiktor.markowski81@gmail.com>
2026-05-06 15:16:57 +0200

    Create response and define it

--- frontend/src/Meetings/Index.tsx
+++ frontend/src/Meetings/Index.tsx
@@ -1,5 +1,31 @@
+import { useState, useEffect } from "react";
+import type { ResponseType } from "../api/ResponseType";
+
+import { ApiGetAllMeetings } from "../api/client";
+import HandleResponse from "../api/HandleResponse";
+
 export default function Index()
 {
+	const [response, setResponse] = useState<ResponseType|null>(null);
+
+	useEffect(() => {
+		const loadMeetings = async () =>
+	{
+		try
+		{
+			const response = await ApiGetAllMeetings();
+
+			setResponse(HandleResponse(response));
+		}
+		catch (error)
+		{
+			console.log("Error submitting data: ", error);
+		}
+	}
+
+	loadMeetings();
+	}, [setResponse]);
+
 	return (
 		<>
 			Hello in meetings index
