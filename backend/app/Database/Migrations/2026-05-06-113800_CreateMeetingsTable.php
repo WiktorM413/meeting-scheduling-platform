1ddfe824f42870008a17e281eb74a13ea1d79f25
Wiktor <wiktor.markowski81@gmail.com>
2026-05-06 14:00:01 +0200

    Create a migration to add meetings table

---
+++ backend/app/Database/Migrations/2026-05-06-113800_CreateMeetingsTable.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateMeetingsTable extends Migration
+{
+	public function up()
+	{
+		$this->db->query("
+			CREATE TABLE `meetings` (
+				`unique_id`   int NOT NULL,
+				`provider_id` int DEFAULT NULL,
+				`receiver_id` int DEFAULT NULL,
+				`topic`       text DEFAULT NULL,
+				`when`        date DEFAULT NULL,
+				`time_start`  varchar(5) DEFAULT NULL,
+				`time_end`    varchar(5) DEFAULT NULL
+			)
+		");
+
+		$this->db->query("
+			ALTER TABLE `meetings`
+				ADD PRIMARY KEY (`unique_id`),
+				ADD KEY `fk_meetings_provider_id` (`provider_id`),
+				ADD KEY `fk_meetings_receiver_id` (`receiver_id`);
+		");
+
+		$this->db->query("
+			ALTER TABLE `meetings`
+  				MODIFY `unique_id` int NOT NULL AUTO_INCREMENT;
+		");
+
+		$this->db->query("
+			ALTER TABLE `meetings`
+				ADD CONSTRAINT `fk_meetings_provider_id`                      FOREIGN KEY (`provider_id`) REFERENCES `users` (`id`),
+				ADD CONSTRAINT `fk_meetings_receiver_id`                      FOREIGN KEY (`receiver_id`) REFERENCES `users` (`id`),
+				ADD CONSTRAINT `chk_meetings_fk_provider_receiver_difference` CHECK (`provider_id` <> `receiver_id`);
+		");
+	}
+
+	public function down()
+	{
+	}
+}
