064d9b7b43783c19699e7dc088adfee68fdd1dcd
Wiktor <wiktor.markowski81@gmail.com>
2026-05-07 15:43:41 +0200

    Add validation rules for a meeting

--- backend/app/Validation/MeetingsValidationRules.php
+++ backend/app/Validation/MeetingsValidationRules.php
@@ -14,4 +14,28 @@ class MeetingsValidationRules extends BaseValidationRules
 			]
 		]
 	];
+
+	public const meeting = [
+		'provider_id' => [
+			'rules' => 'required|integer'
+		],
+		'receiver_id' => [
+			'rules' => 'required|integer'
+		],
+		'topic' => [
+			'rules' => 'required|string|max_length[255]'
+		],
+		'when' => [
+			'rules' => 'required|valid_date[Y-m-d]'
+		],
+		'where' => [
+			'rules' => 'required|string|max_length[255]'
+		],
+		'time_start' => [
+			'rules' => 'required|valid_date[H:i]'
+		],
+		'time_end' => [
+			'rules' => 'required|valid_date[H:i]'
+		]
+	];
 }
