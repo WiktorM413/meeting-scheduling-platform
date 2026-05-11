bfb2a5841c0901d672ad0189f59dced42fa81082
Wiktor <wiktor.markowski81@gmail.com>
2026-05-11 19:19:33 +0200

    Add displaying the day

--- frontend/src/Components/MspDayDisplay/MspDayDisplay.tsx
+++ frontend/src/Components/MspDayDisplay/MspDayDisplay.tsx
@@ -1,6 +1,8 @@
 import { useMemo, useState } from "react";
 import type { MeetingType } from "../../api/MeetingType"
 import type { DayCell } from "../MspCalendar/DayCell";
+import { FormatTime } from "../../utils/time";
+import { popup } from "../MspPopup/PopupManager";
 
 function BuildDay(year: number, month: number, day: number, meetings?: MeetingType[]): DayCell
 {
@@ -44,6 +46,59 @@ function BuildDay(year: number, month: number, day: number, meetings?: MeetingTy
 	return cell;
 }
 
+type DayObjPorps =
+{
+	cell:      DayCell;
+	year:      number;
+	month:     number;
+	day:       number;
+	meetings?: MeetingType[];
+}
+
+function DayObj({cell, year, month, meetings}: DayObjPorps)
+{
+	if (!cell)
+		{
+			return <div className="msp-week-display-cell msp-week-display-cell-empty"/>
+		}
+
+		return (
+			<div
+				className={`msp-week-display-cell ${
+					cell.isSelected ? "msp-week-display-cell-selected" : ""
+				} ${
+					cell?.isToday ? "msp-week-display-cell-today" : ""
+				} ${
+					cell?.availability ? "msp-week-display-cell-available": "msp-week-display-cell-blocked"
+				}`}
+			>
+				<span className="msp-week-display-cell-date">{cell.date}</span>
+				{cell.hasEvents && <span className="msp-calendar-cell-dot"
+					onClick={() =>
+					{
+						const dateString = `${year}-${String(month + 1).padStart(2, '0')}-${String(cell.date).padStart(2, '0')}`;
+						
+						popup.Open(
+							<>
+								<h1>{dateString}</h1>
+								{meetings?.map((meeting, i) =>
+									meeting.when === dateString ?
+									(
+										<div key={i}>
+											{meeting.other_names}&nbsp;
+											({FormatTime(meeting.time_start)} - {FormatTime(meeting.time_end)}):&nbsp;
+											{meeting.topic}
+										</div>
+									) : null
+								)}
+							</>
+						);
+					}
+				}/>}
+			</div>
+		)
+}
+
 type MspDayDisplayProps =
 {
 	meetings?: MeetingType[];
@@ -55,12 +110,25 @@ export default function MspDayDisplay({meetings}: MspDayDisplayProps)
 	const [year,  setYear]  = useState(now.getFullYear());
 	const [month, setMonth] = useState(now.getMonth());
 	const [day,   setDay]   = useState(now.getDate());
+	const [date,  setDate]  = useState("");
 
 	const cell = useMemo(() => BuildDay(year, month, day, meetings), [year, month, day, meetings]);
-
+	
 	return (
 		<div className="msp-day-display">
-			<input type="date" onChange={(e) => console.log(e.target.value)}/>
+			<div className="msp-day-display-header">
+				<input type="date" onChange={(e) =>
+					{
+						const [inYear, inMonth, inDay] = e.target.value.split("-");
+
+						setYear (Number(inYear));
+						setMonth(Number(inMonth));
+						setDay  (Number(inDay));
+					}
+				}/>
+			</div>
+
+			<DayObj cell={cell} year={year} month={month} day={day} meetings={meetings}/>
 		</div>
 	);
 }
