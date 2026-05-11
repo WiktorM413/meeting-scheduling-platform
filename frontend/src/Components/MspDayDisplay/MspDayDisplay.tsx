26e79dbc2e8b0ee6cb5dfba6b4fe3a04ab84f116
Wiktor <wiktor.markowski81@gmail.com>
2026-05-11 21:14:19 +0200

    Remove unneeded functions and variables

--- frontend/src/Components/MspDayDisplay/MspDayDisplay.tsx
+++ frontend/src/Components/MspDayDisplay/MspDayDisplay.tsx
@@ -1,48 +1,7 @@
 import "./style.scss";
-import { useMemo, useState } from "react";
+import { useState } from "react";
 import type { MeetingType } from "../../api/MeetingType"
-import type { DayCell } from "../MspCalendar/DayCell";
 import { FormatTime } from "../../utils/time";
-
-function BuildDay(date: string, meetings?: MeetingType[]): DayCell
-{
-	const [year, month, day] = date.split("-").map((value) => Number(value));
-
-	const today   = new Date();
-	const dateObj = new Date(year, month - 1 /* to month index */, day);
-	const isToday = 
-		dateObj.getDate()     === today.getDate()  &&
-		dateObj.getMonth()    === today.getMonth() &&
-		dateObj.getFullYear() === today.getFullYear();
-
-	const d = (dateObj.getDay() + 6) % 7; // Convert from 0 -> Sunday to 0 -> Monday
-	const isAvailable = d < 5;
-
-	let hasEvents = false;
-
-	if (meetings)
-	{
-		for (let i = 0; i < meetings.length; i++)
-		{
-			if (meetings[i].when === date)
-			{
-				hasEvents = true;
-				break;
-			}
-		}
-	}
-
-	const cell: DayCell =
-	{
-		date:         day,
-		isToday:      isToday,
-		isSelected:   false,
-		hasEvents:    hasEvents,
-		availability: isAvailable
-	}
-
-	return cell;
-}
 
 type DayObjPorps =
 {
@@ -94,10 +53,7 @@ export default function MspDayDisplay({meetings}: MspDayDisplayProps)
 		(now.getMonth() + 1).toString().padStart(2, "0") /* from month index to month number */ + "-" +
 		now.getDate().toString().padStart(2, "0");
 	const [date,  setDate]  = useState(today);
-		
-
-	const cell = useMemo(() => BuildDay(date, meetings), [date, meetings]);
-	
+			
 	return (
 		<div className="msp-day-display">
 			<div className="msp-day-display-header">
