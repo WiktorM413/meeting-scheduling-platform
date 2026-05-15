9c504ab861690d7eba2eb862ef80ba725612bfdd
Wiktor <wiktor.markowski81@gmail.com>
2026-05-15 21:23:14 +0200

    Add the button in form of a logo to go to your profile

--- frontend/src/Templates/Header/AccountButtons.tsx
+++ frontend/src/Templates/Header/AccountButtons.tsx
@@ -1,6 +1,8 @@
 import "./style.scss"
 import { useNavigate } from "react-router-dom";
 import MspAnchor from "../../Components/MspAnchor";
+import { useAuth } from "../../context/AuthContext";
+import defaultProfilePic from "../../assets/default-profile-pic.svg";
 
 type AccountButtonsProps =
 {
@@ -9,13 +11,20 @@ type AccountButtonsProps =
 
 export default function AccountButtons({ isAuthenticated }: AccountButtonsProps)
 {
-	const navigate = useNavigate();
+	const navigate     = useNavigate();
+	const { userData } = useAuth();
+	console.log(defaultProfilePic);
 
 	return (
 		<div className="msp-account-buttons">
 			{isAuthenticated ?
 			(
 				<>
+					<div>
+						<MspAnchor label="" navigator={navigate} to={`userProfile/${userData?.id}`}>
+							<img className="msp-account-buttons-profile-pic" src={defaultProfilePic} alt="Profile"/>
+						</MspAnchor>
+					</div>
 					<MspAnchor label="Log out" navigator={navigate} to="logout" className="msp-accout-logout"/>
 				</>
 			)
