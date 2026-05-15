a0689029aee25c5a8894db32f51fd8994ed71102
Wiktor <wiktor.markowski81@gmail.com>
2026-05-15 21:26:37 +0200

    Add basic retrieving params from url

--- frontend/src/User/UserProfile.tsx
+++ frontend/src/User/UserProfile.tsx
@@ -1,8 +1,14 @@
+import { useParams } from "react-router-dom"
+import { useAuth } from "../context/AuthContext";
+
 export default function UserProfile()
 {
+	const { userId } = useParams();
+	const { userData } = useAuth();
+
 	return (
 		<div className="msp-user-profile">
-			
+			Hello {userId} or rather {userData?.id}
 		</div>
 	)
 }
