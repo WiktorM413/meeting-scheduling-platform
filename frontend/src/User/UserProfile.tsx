6c7ae438971a1d43474f2d01ef95925930ccedb9
Wiktor <wiktor.markowski81@gmail.com>
2026-05-16 10:52:14 +0200

    loadUser based on uerId param

--- frontend/src/User/UserProfile.tsx
+++ frontend/src/User/UserProfile.tsx
@@ -1,14 +1,63 @@
 import { useParams } from "react-router-dom"
 import { useAuth } from "../context/AuthContext";
+import { useEffect, useState } from "react";
+import type { UserData } from "../api/UserType";
+import { ApiGetUserById } from "../api/client";
+import HandleResponse from "../api/HandleResponse";
 
 export default function UserProfile()
 {
-	const { userId } = useParams();
+	const { userId }   = useParams();
 	const { userData } = useAuth();
 
+	if (! userId)
+	{
+		return (
+			<div className="msp-user-profile">
+				<h1 className="msp-error">Wrong url</h1>
+			</div>
+		)
+	}
+
+	const [user,          setUser]          = useState<UserData|null>(null);
+	const [isCurrentUser, setIsCurrentUser] = useState(false);
+
+	useEffect(() =>
+	{
+		const loadUser = async () =>
+		{
+			if (Number(userId) == userData?.id)
+			{
+				setUser(userData);
+				setIsCurrentUser(true);
+			}
+			else
+			{
+				try
+				{
+					const response = await ApiGetUserById(Number(userId));
+					const handled  = HandleResponse(response);
+
+					if (handled.type === "success")
+					{
+						setUser(handled.data);
+						setIsCurrentUser(false);
+					}
+				}
+				catch (error)
+				{
+					console.log("Error retrieving user:", error);
+				}
+			}
+		}
+
+		loadUser();
+	}, [userId]);
+	
 	return (
 		<div className="msp-user-profile">
-			Hello {userId} or rather {userData?.id}
+			<h1 className="msp-user-profile-header">{isCurrentUser ? "Hello" : "Meet"} {user?.first_name} {user?.last_name}.</h1>
+			
 		</div>
 	)
 }
