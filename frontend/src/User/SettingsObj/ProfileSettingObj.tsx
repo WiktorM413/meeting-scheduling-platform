9c8b7c7f06c65f8bb20268297fa74e60c0ac791a
Wiktor <wiktor.markowski81@gmail.com>
2026-05-18 17:59:17 +0200

    Create the html for profile setting obj and give it a template css

--- frontend/src/User/SettingsObj/ProfileSettingObj.tsx
+++ frontend/src/User/SettingsObj/ProfileSettingObj.tsx
@@ -1,8 +1,87 @@
+import "./style.scss";
+import { useState } from "react";
+import MspButton from "../../Components/MspButton";
+import MspFileInput from "../../Components/MspFileInput/MspFileInput";
+import MspUserProfilePic from "../../Components/MspUserProfilePic";
+import { useAuth } from "../../context/AuthContext";
+import MspFormField from "../../Components/MspFormField";
+
+
+async function SaveChanges(profilePic: File|null, firstname: string, lastname: string, email: string)
+{
+	const imageAsBytes = async (): Promise<string|null> =>
+	{
+		if (! profilePic)
+		{
+			return null;
+		}
+
+		const buffer = await profilePic.arrayBuffer();
+		const bytes = new Uint8Array(buffer);
+
+		return bytes.toString();
+	}
+	
+	const profilePicString: string|null = await imageAsBytes();
+
+	
+	
+}
+
 export default function ProfileSettingObj()
 {
+	const { userData } = useAuth();
+
+	if (! userData)
+	{
+		return <div className="msp-profile-setting-obj">Your user doesn't exist!</div>
+	}
+
+	const [profilePic,    setProfilePic]    = useState<File|null>(null);
+	const [profilePicURL, setProfilePicURL] = useState<string|null>(null);
+	const [firstname,     setFirstname]     = useState<string>(userData.first_name);
+	const [lastname,      setLastname]      = useState<string>(userData.last_name);
+	const [email,         setEmail]         = useState<string>(userData.email);
+
+
+	const handleProfilePicChange = (file: File | null) =>
+	{
+		setProfilePic(file);
+
+		if (profilePicURL)
+		{
+			URL.revokeObjectURL(profilePicURL);
+		}
+
+		setProfilePicURL(file ? URL.createObjectURL(file) : null);
+	}
+
 	return (
-		<>
-			Hello from Profile setting
-		</>
+		<div className="msp-profile-setting-obj">
+			<div className="msp-profile-setting-obj-header">
+				<h2>Profile</h2>
+				<p className="msp-small-text">Update your personal information and public-facing details.</p>
+			</div>
+			<div className="msp-profile-setting-obj-profile-pic">
+				<MspUserProfilePic setImage={profilePicURL ?? userData.profile_pic}/>
+				<div className="msp-profile-setting-obj-profile-pic-options">
+					<p>Profile photo</p>
+					<MspFileInput label="Upload new" valueSetter={setProfilePic} onChange={handleProfilePicChange}/>
+					<MspButton label="Remove" onClick={() => handleProfilePicChange(null)}/>
+				</div>
+			</div>
+			<div className="msp-profile-setting-obj-credentials">
+				<div className="msp-profile-setting-obj-credentials-name">
+					<MspFormField label="First name" value={firstname} setter={setFirstname}/>
+					<MspFormField label="Last name"  value={lastname}  setter={setLastname}/>
+				</div>
+				<div className="msp-profile-setting-obj-credentials-email">
+					<MspFormField label="Email address" value={email} setter={setEmail}/>
+				</div>
+			</div>
+			<div className="msp-profile-setting-obj-save-button">
+				<MspButton label="Save changes" onClick={() => SaveChanges(profilePic, firstname, lastname, email)}/>
+			</div>
+		</div>
 	);
 }
