096ac4604b55e8eb726217de01d648667dc77236
Wiktor <wiktor.markowski81@gmail.com>
2026-05-19 19:22:23 +0200

    Fix MspUserProfilePic to display the base64 profile

--- frontend/src/Components/MspUserProfilePic.tsx
+++ frontend/src/Components/MspUserProfilePic.tsx
@@ -7,6 +7,27 @@ type MspUserProfileProps =
 	setImage?:  string;
 }
 
+function GetImageExtension(base64Img: string): string
+{
+	if (base64Img.startsWith('/9j/'))
+	{
+		return 'image/jpeg';
+	}
+	if (base64Img.startsWith('iVBOR')) 
+	{
+		return 'image/png';
+	}
+	if (base64Img.startsWith('R0lGO')) 
+	{
+		return 'image/gif';
+	}
+	if (base64Img.startsWith('UklGR')) 
+	{
+		return 'image/webp';
+	}
+	return 'image/jpeg';
+}
+
 export default function MspUserProfilePic({className, setImage}: MspUserProfileProps)
 {
 	const { userData } = useAuth();
@@ -16,7 +37,16 @@ export default function MspUserProfilePic({className, setImage}: MspUserProfileP
 		setImage = userData?.profile_pic;
 	}
 
+	let src = defaultProfilePic;
+
+	if (userData?.profile_pic)
+	{
+		src = `data:${GetImageExtension(userData.profile_pic)};base64,${userData.profile_pic}`;
+	}
+	
+	console.log(src);
+	
 	return (
-		<img className={`msp-user-profile-pic ${className ? className : ""}`} src={setImage ? setImage : defaultProfilePic} alt="Profile"/>
+		<img className={`msp-user-profile-pic ${className ? className : ""}`} src={src} alt="Profile"/>
 	);
 }
