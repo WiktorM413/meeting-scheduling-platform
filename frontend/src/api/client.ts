77565c9f3b47e93ee7ad51f44d745919048423c3
Wiktor <wiktor.markowski81@gmail.com>
2026-05-22 14:00:44 +0200

    Create function ApiUpdateUserPassword

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -200,3 +200,17 @@ export async function ApiUpdateUserSettings(userId: number, publicProfile?: numb
 
 	return response;
 }
+
+export async function ApiUpdateUserPassword(userId: number, currentPassword: string, newPassword: string, repeatPassword: string)
+{
+	const response = await api.post("/updatePassword",
+		{
+			"user_id":          userId,
+			"current_password": currentPassword,
+			"new_password":     newPassword,
+			"repeat_password":  repeatPassword
+		}
+	)
+
+	return response;
+}
