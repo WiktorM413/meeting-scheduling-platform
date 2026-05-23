e8f56e08398e17f4e6321df11925124d548f4e83
Wiktor <wiktor.markowski81@gmail.com>
2026-05-23 08:16:59 +0200

    Add the search input and filteredUsers

--- frontend/src/User/UserSearch.tsx
+++ frontend/src/User/UserSearch.tsx
@@ -1,14 +1,41 @@
+import { useEffect, useState } from "react";
+import MspButton from "../Components/MspButton";
+import { type UserData } from "../api/UserType";
+
 export default function UserSearch()
 {
+	const [userSearch,    setUserSearch]    = useState("");
+	const [filteredUsers, setFilteredUsers] = useState<UserData[]>([]);
+	
+	useEffect(() =>
+	{
+		const searchUsers = async () =>
+		{
+			
+		}
+		
+		setTimeout(searchUsers, 300);
+	}, [userSearch]);
+	
 	return (
 		<div className="msp-user-search">
 			<div className="msp-user-search-header">
 				<h1>Browse through users</h1>
 				<p>Find people you want to meet with</p>
 			</div>
-			<div>
-				
+			<div className="msp-user-search-search-wrapper">
+				<input 
+					type="text"
+					value={userSearch}
+					onChange={(e) => setUserSearch(e.target.value)}
+					placeholder="Browse users..."
+					className="msp-user-search-search"
+				/>
+				{userSearch &&
+					<MspButton label="✕" className="msp-user-search-search-clear" onClick={() => setUserSearch("")}/>
+				}
 			</div>
+			
 		</div>
 	);
 }
