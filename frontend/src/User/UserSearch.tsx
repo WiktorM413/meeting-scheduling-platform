eec92c6d43b846e382aeb93c3814e9eb4830136d
Wiktor <wiktor.markowski81@gmail.com>
2026-05-23 08:47:50 +0200

    Add searching the users and displaying themc

--- frontend/src/User/UserSearch.tsx
+++ frontend/src/User/UserSearch.tsx
@@ -3,9 +3,12 @@ import MspButton from "../Components/MspButton";
 import { type UserData } from "../api/UserType";
 import { ApiGetPublicUsersLike } from "../api/client";
 import HandleResponse from "../api/HandleResponse";
+import { useNavigate } from "react-router-dom";
 
 export default function UserSearch()
 {
+	const navigate = useNavigate();
+	
 	const [userSearch,    setUserSearch]    = useState("");
 	const [filteredUsers, setFilteredUsers] = useState<UserData[]>([]);
 	
@@ -15,6 +18,7 @@ export default function UserSearch()
 		{
 			if (! userSearch)
 			{
+				setFilteredUsers([]);
 				return;
 			}
 
@@ -56,7 +60,23 @@ export default function UserSearch()
 					<MspButton label="✕" className="msp-user-search-search-clear" onClick={() => setUserSearch("")}/>
 				}
 			</div>
-			
+			{userSearch.trim() && filteredUsers.length > 0 &&
+				<div className="msp-user-search-user-results">
+					{
+						filteredUsers.map((user) =>
+						(
+							<div
+								key={user.id}
+								className="msp-user-search-user-result"
+								onClick={() => navigate(`/userProfile/${user.id}`)}
+							>
+								<h4>{user.email}</h4>
+								<p>{user.first_name} {user.last_name}</p>
+							</div>
+						))
+					}
+				</div>
+			}
 		</div>
 	);
 }
