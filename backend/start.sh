114a42906e6bb841a87d191c4405d4e33ed0977f
Wiktor <wiktor.markowski81@gmail.com>
2026-06-03 18:12:56 +0200

    Generate the variables in .env again

--- backend/start.sh
+++ backend/start.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# cat > .env << EOF
-# CI_ENVIRONMENT=development
+cat > .env << EOF
+CI_ENVIRONMENT=development
 
-# database.default.hostname=${MYSQLHOST}
-# database.default.database=${MYSQLDATABASE}
-# database.default.username=${MYSQLUSER}
-# database.default.password=${MYSQLPASSWORD}
-# database.default.port=${MYSQLPORT}
-# database.default.DBDriver=MySQLi
-# EOF
+database.default.hostname=${MYSQLHOST}
+database.default.database=${MYSQLDATABASE}
+database.default.username=${MYSQLUSER}
+database.default.password=${MYSQLPASSWORD}
+database.default.port=${MYSQLPORT}
+database.default.DBDriver=MySQLi
+EOF
 
 php -S 0.0.0.0:${PORT} -t public
