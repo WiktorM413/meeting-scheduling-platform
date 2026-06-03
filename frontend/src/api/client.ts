dff7cea9708e7046d9378e207c4cec94223f32c0
Wiktor <wiktor.markowski81@gmail.com>
2026-06-03 16:45:53 +0200

    Replace localhost links with the actual ones

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -3,7 +3,7 @@ import HandleResponse from "./HandleResponse";
 
 export const api = axios.create
 ({
-	baseURL: "meeting-scheduling-platform-production.up.railway.app/api",
+	baseURL: "https://meeting-scheduling-platform-production.up.railway.app/api",
 	withCredentials: true,
 });
 
