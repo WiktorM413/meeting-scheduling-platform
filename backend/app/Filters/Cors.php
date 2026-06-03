4eaff48f88d1ff43c332433851dfe8554adc53ed
Wiktor <wiktor.markowski81@gmail.com>
2026-06-03 16:50:45 +0200

    Add cors

---
+++ backend/app/Filters/Cors.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Filters;
+
+use CodeIgniter\Filters\FilterInterface;
+use CodeIgniter\HTTP\RequestInterface;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class Cors implements FilterInterface
+{
+	public function before(RequestInterface $request, $arguments = null)
+	{
+		$response = service('response');
+
+		$response->setHeader('Access-Control-Allow-Origin', 'https://meeting-scheduling-platform.wiktor-markowski362.workers.dev');
+		$response->setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept, Origin');
+		$response->setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+
+		// IMPORTANT: handle preflight immediately
+		if ($request->getMethod() === 'options') {
+			return $response->setStatusCode(200);
+		}
+	}
+
+	public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)
+	{
+		return $response;
+	}
+}
