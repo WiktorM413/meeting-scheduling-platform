3e1f6b5e3d366d1953c939d0b94336142b73a61a
Wiktor <wiktor.markowski81@gmail.com>
2026-06-03 17:43:57 +0200

    Change codeigniter back to production mode

--- backend/start.sh
+++ backend/start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+
 cat > .env << EOF
-CI_ENVIRONMENT=development
+CI_ENVIRONMENT=production
+
 database.default.hostname=${MYSQLHOST}
 database.default.database=${MYSQL_DATABASE}
 database.default.username=${MYSQLUSER}
@@ -9,5 +11,6 @@ database.default.port=${MYSQLPORT}
 database.default.DBDriver=MySQLi
 EOF
 
-php spark migrate
-php spark serve --host 0.0.0.0 --port $PORT
+php spark migrate --all
+
+php -S 0.0.0.0:$PORT -t public
