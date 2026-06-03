bf0314cc8c839b401baf1fedd5a68c627e74ea0e
Wiktor <wiktor.markowski81@gmail.com>
2026-06-03 18:04:33 +0200

    Fix typos hopefully

--- backend/start.sh
+++ backend/start.sh
@@ -4,11 +4,11 @@ cat > .env << EOF
 CI_ENVIRONMENT=development
 
 database.default.hostname=${MYSQLHOST}
-database.default.database=${MYSQL_DATABASE}
+database.default.database=${MYSQLDATABASE}
 database.default.username=${MYSQLUSER}
 database.default.password=${MYSQLPASSWORD}
 database.default.port=${MYSQLPORT}
 database.default.DBDriver=MySQLi
 EOF
 
-php -S 0.0.0.0:$PORT -t public
+php -S 0.0.0.0:${MYSQLPORT} -t public
